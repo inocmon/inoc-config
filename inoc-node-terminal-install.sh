f7c4a6f32eae4e005ca9773e2421b5e649b08706
inocmon <rinaldopvaz@gmail.com>
2025-04-21 12:10:09 -0300

    novo instalador node temrinal

--- inoc-node-terminal-install.sh
+++ inoc-node-terminal-install.sh
@@ -1,11 +1,61 @@
 #!/bin/bash
-#set -xv
-# Cria o diretório "rsa" apenas se ele não existir
-[ ! -d rsa ] && mkdir rsa
-[ ! -d rsa ] && mkdir logs
+
+# Processando argumentos com getopt
+OPTIONS=$(getopt -o '' --long username:,apikey:,refresh-certs-force -- "$@")
+eval set -- "$OPTIONS"
+
+userName=""
+apiKey=""
+REFRESH_CERTS_FORCE=false
+
+while true; do
+  case "$1" in
+    --username)
+      userName="$2"
+      shift 2
+      ;;
+    --apikey)
+      apiKey="$2"
+      shift 2
+      ;;
+    --refresh-certs-force)
+      REFRESH_CERTS_FORCE=true
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Erro: Argumento inválido: $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Validação de argumentos obrigatórios
+if [ -z "$userName" ] || [ -z "$apiKey" ]; then
+    echo "Uso: $0 --username Nome_do_usuario --apikey 12345 [--refresh-certs-force]"
+    exit 1
+fi
+
+# Cria diretórios se não existirem
+mkdir -p rsa logs
 [ ! -f .env ] && touch .env
 
-# Gera a chave RSA apenas se ela não existir
+# Inserindo dados no arquivo .env
+# Atualiza ou adiciona NODE_TERMINAL_API_KEY no .env
+if grep -q '^NODE_TERMINAL_API_KEY=' /opt/inoc-config/.env; then
+    sed -i "s/^NODE_TERMINAL_API_KEY=.*/NODE_TERMINAL_API_KEY=${apiKey}/" /opt/inoc-config/.env
+else
+    echo "NODE_TERMINAL_API_KEY=${apiKey}" >> /opt/inoc-config/.env
+fi
+
+
+# Atualizando o usuário no arquivo de serviço
+sed -i "s/^User=.*/User=${userName}/" /opt/inoc-config/inoc-node-terminal.service
+
+# Gerar chave RSA se necessário
 if [ ! -f rsa/keypair.pem ]; then
     echo "Gerando nova chave RSA..."
     ssh-keygen -t rsa -b 2048 -m PEM -f rsa/keypair.pem -q -N ""
@@ -14,16 +64,10 @@ else
 fi
 
 echo "# Conteúdo da chave pública:"
-echo "#"
 cat rsa/keypair.pem.pub
 
-# Instalando certificados
+# Instalando certificados SSL
 mkdir -p /opt/inoc-config/certs
-
-REFRESH_CERTS_FORCE=false
-if [ "$1" = "--refresh-certs-force" ]; then
-    REFRESH_CERTS_FORCE=true
-fi
 
 if [ "$REFRESH_CERTS_FORCE" = true ] || [ ! -f /opt/inoc-config/certs/server.key ] || [ ! -f /opt/inoc-config/certs/server.cert ]; then
     echo "Gerando novo certificado SSL..."
@@ -42,11 +86,10 @@ fi
 
 # Copiar arquivos e configuração do serviço
 chmod +x /opt/inoc-config/inoc-node-terminal
-cp inoc-node-terminal inoc-node-terminal.tmp
-mv inoc-node-terminal.tmp /usr/bin/inoc-node-terminal
+chown -R "${userName}:${userName}" /opt/inoc-config
 
-cp inoc-node-terminal.service inoc-node-terminal.service.tmp
-mv inoc-node-terminal.service.tmp /etc/systemd/system/inoc-node-terminal.service
+cp inoc-node-terminal /usr/bin/inoc-node-terminal
+cp inoc-node-terminal.service /etc/systemd/system/inoc-node-terminal.service
 
 systemctl daemon-reload
 systemctl enable inoc-node-terminal
