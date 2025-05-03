59f4a4e662fc038ac4cc7df30c4760aebbcd6726
inocmon <rinaldopvaz@gmail.com>
2025-05-03 17:26:10 -0300

    com php mais recente

--- librenms-install.sh
+++ librenms-install.sh
@@ -58,7 +58,10 @@ apt install -y software-properties-common curl apt-transport-https ca-certificat
 # Instala PHP 8.2
 add-apt-repository -y ppa:ondrej/php
 apt update
-apt install -y  snmp snmpd apache2 php8.2 php8.2-cli php8.2-common php8.2-curl php8.2-fpm php8.2-gd php8.2-gmp php8.2-json php8.2-mbstring php8.2-mysql php8.2-snmp php8.2-xml php8.2-zip libapache2-mod-php8.2
+apt install -y apache2
+apt install -y software-properties-common
+add-apt-repository -y ppa:ondrej/php
+apt install -y snmp snmpd apache2 php php-cli php-common php-curl php-fpm php-gd php-gmp php-mbstring php-mysql php-snmp php-xml php-zip libapache2-mod-php
 
 
 update-alternatives --set php /usr/bin/php8.2
